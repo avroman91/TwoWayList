9eaed74fc448c395723501e574c73f9fd16f75ab
Roman Avdeienko <avroman91@gmail.com>
2021-08-17 01:54:19 +0300

    tests commented //;

--- src/main/java/ua/ithillel/twoWayList/Main.java
+++ src/main/java/ua/ithillel/twoWayList/Main.java
@@ -4,30 +4,30 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        TwoWayList<Integer> list = new TwoWayList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.addFirst(0);
-        TwoWayList<Integer> list2 = new TwoWayList();
-        list2.add(11);
-        list2.add(22);
-        list.addAll(2, list2);
+//        TwoWayList<Integer> list = new TwoWayList();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.addFirst(0);
+//        TwoWayList<Integer> list2 = new TwoWayList();
+//        list2.add(11);
+//        list2.add(22);
+//        list.addAll(2, list2);
 //        list.removeByIndex(0);
 //        list.remove(11);
-        System.out.println(list.contains(1));
-        list.set(2, 10);
-
-
-        Iterator<Integer> iter = list.iterator();
-        System.out.println("----------------------");
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
-        }
-        System.out.println("----------------------");
-        Iterator<Integer> reverseIter = list.reverseIterator();
-        while (reverseIter.hasNext()) {
-            System.out.println(reverseIter.next());
-        }
+//        System.out.println(list.contains(1));
+//        list.set(2, 10);
+//
+//
+//        Iterator<Integer> iter = list.iterator();
+//        System.out.println("----------------------");
+//        while (iter.hasNext()) {
+//            System.out.println(iter.next());
+//        }
+//        System.out.println("----------------------");
+//        Iterator<Integer> reverseIter = list.reverseIterator();
+//        while (reverseIter.hasNext()) {
+//            System.out.println(reverseIter.next());
+//        }
     }
 }
